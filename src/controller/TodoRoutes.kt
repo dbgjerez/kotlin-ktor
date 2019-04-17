d63f5060e22a475bcb1d7b23ca026d463a2d888f
David Borrego <dbgjerez@gmail.com>
2019-04-17 13:47:59 +0200

    crud complete

--- src/controller/TodoRoutes.kt
+++ src/controller/TodoRoutes.kt
@@ -1,18 +1,34 @@
 package com.dbg.controller
 
+import com.dbg.dto.TodoDTO
 import com.dbg.service.TodoService
 import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.request.receiveOrNull
 import io.ktor.response.respond
-import io.ktor.routing.get
-import io.ktor.routing.routing
+import io.ktor.routing.*
 import javax.inject.Inject
 
 class TodoRoutes @Inject constructor(application: Application, todoService: TodoService) {
     init {
         application.routing {
             get("/todo") {
-                call.respond(todoService.findAll())
+                call.respond(HttpStatusCode.OK, todoService.findAll())
+            }
+            get("/todo/{id}") {
+                call.parameters["id"].let { todoService::findById }.let { call.respond(HttpStatusCode.OK, it) }
+            }
+            post("/todo") {
+                call.receiveOrNull<TodoDTO>().let { todoService::create }
+                    .let { call.respond(HttpStatusCode.Created, it) }
+            }
+            put("/todo/{id}") {
+                val id = call.parameters["id"]
+                call.receiveOrNull<TodoDTO>().let { todoService.update(id, it) }.let { call.respond(HttpStatusCode.OK) }
+            }
+            delete("/todo/{id}") {
+                call.parameters["id"].let { todoService::delete }.let { call.respond(HttpStatusCode.OK) }
             }
         }
     }
