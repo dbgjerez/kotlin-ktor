9f9b1faf42dc7ec8962b765debddaccae8081e16
David Borrego <dbgjerez@gmail.com>
2019-04-17 20:43:36 +0200

    service callin dao

--- src/service/TodoService.kt
+++ src/service/TodoService.kt
@@ -1,14 +1,16 @@
 package com.dbg.service
 
+import com.dbg.dao.TodoRepository
 import com.dbg.dto.TodoDTO
-import com.google.gson.Gson
+import com.dbg.mapper.TodoMapper
 import com.google.inject.Inject
 
-class TodoService @Inject constructor(private val gson: Gson) {
+class TodoService @Inject constructor(private val todoRepository: TodoRepository, private val todoMapper: TodoMapper) {
 
-    fun findAll(): TodoDTO {
-        val t = """{"id": "prueba", "title":"hola"}"""
-        return gson.fromJson(t, TodoDTO::class.java)
-    }
+    fun findAll(): List<TodoDTO> = todoRepository.findAll().mapNotNull(todoMapper::map)
+    fun findById(id: String?): TodoDTO? = todoRepository.findbyId(id).let(todoMapper::map)
+    fun create(todo: TodoDTO?): TodoDTO? = todo.let(todoMapper::map).let(todoRepository::create).let(todoMapper::map)
+    fun update(id: String?, todo: TodoDTO?) = todoRepository.update(id, todo)
+    fun delete(id: String?) = id.let(todoRepository::delete)
 
 }
