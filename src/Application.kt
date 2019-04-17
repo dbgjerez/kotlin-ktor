ae2c0a1503acefd3742fb092e229a523599d15fb
David Borrego <dbgjerez@gmail.com>
2019-04-17 14:02:11 +0200

    more clean configuration

--- src/Application.kt
+++ src/Application.kt
@@ -3,10 +3,6 @@ package com.dbg
 import com.dbg.config.MainModule
 import com.google.inject.Guice
 import io.ktor.application.Application
-import io.ktor.application.install
-import io.ktor.features.ContentNegotiation
-import io.ktor.features.DefaultHeaders
-import io.ktor.gson.gson
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -16,9 +12,5 @@ fun Application.module(testing: Boolean = false) {
 
     Guice.createInjector(MainModule(this))
 
-    install(DefaultHeaders)
-    install(ContentNegotiation) {
-        gson { }
-    }
 }
 
--- src/config/MainModule.kt
+++ src/config/MainModule.kt
@@ -2,21 +2,27 @@ package com.dbg.config
 
 import com.dbg.controller.TodoRoutes
 import com.dbg.service.TodoService
-import com.google.gson.Gson
 import com.google.inject.AbstractModule
-import com.google.inject.Provides
-import com.google.inject.Singleton
 import io.ktor.application.Application
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.gson.gson
+import java.text.DateFormat
 
 class MainModule(private val application: Application) : AbstractModule() {
     override fun configure() {
+        application.install(ContentNegotiation) {
+            gson {
+                setDateFormat(DateFormat.LONG)
+                setPrettyPrinting()
+            }
+        }
+        application.install(DefaultHeaders)
+
         bind(TodoRoutes::class.java).asEagerSingleton()
         bind(TodoService::class.java).asEagerSingleton()
         bind(Application::class.java).toInstance(application)
     }
-
-    @Provides
-    @Singleton
-    fun gson(): Gson = Gson()
 
 }
