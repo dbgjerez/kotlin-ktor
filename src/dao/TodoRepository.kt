c701310f800e8f4475958511efb793c10234a0bc
David Borrego <dbgjerez@gmail.com>
2019-04-18 12:54:07 +0200

    cassandra cluster configured with docker

--- src/dao/TodoRepository.kt
+++ src/dao/TodoRepository.kt
@@ -1,12 +1,36 @@
 package com.dbg.dao
 
+import com.datastax.oss.driver.api.core.CqlSession
+import com.datastax.oss.driver.api.core.CqlSessionBuilder
+import com.datastax.oss.driver.api.core.type.DataTypes
+import com.datastax.oss.driver.api.querybuilder.SchemaBuilder
 import com.dbg.dto.TodoDTO
 import com.dbg.model.Todo
 import java.util.*
 
 class TodoRepository {
 
-    fun findAll(): List<Todo> = Arrays.asList(Todo("a", "a"))
+    val keyspace = "example"
+    val table = "todo"
+    val session: CqlSession
+
+    init {
+        keyspace()
+        table()
+        session = session()
+    }
+
+    private fun sessionInit() = CqlSessionBuilder().build()
+    private fun session(): CqlSession = CqlSessionBuilder().withKeyspace(keyspace).build()
+    private fun keyspace() = sessionInit().execute(SchemaBuilder.createKeyspace(keyspace).withSimpleStrategy(1).build())
+    private fun table() = session().execute(
+        SchemaBuilder.createTable(keyspace, table).withPartitionKey(
+            "id",
+            DataTypes.TEXT
+        ).withColumn("title", DataTypes.TEXT).build()
+    )
+
+    fun findAll(): List<Todo> = Arrays.asList()
     fun findbyId(id: String?): Todo? = Todo(id, "v")
     fun create(todo: Todo?) = Todo("a", "b")
     fun update(id: String?, todo: TodoDTO?) = {}
